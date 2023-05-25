9bf9242e1393fbaf497c96388f5101712e8b5774
julia <juliavale.estudos@gmail.com>
2023-05-25 15:24:13 -0300

    API Carro

--- aula10/apiCarro/src/server.js
+++ aula10/apiCarro/src/server.js
@@ -1,6 +1,6 @@
 require('dotenv').config({path:'variaveis.env'});
 const express = require('express');
-const cors = require('cors'); 
+const cors = require('cors');
 const bodyParser = require('body-Parser');
 
 const routes = require('./routes');
@@ -8,7 +8,8 @@ const routes = require('./routes');
 const server = express();
 server.use(cors());
 server.use(bodyParser.urlencoded({extended: false}));
+server.use('/api',routes);
 
 server.listen(process.env.PORT,()=>{
-    console.log(`Servidor roda em http://localhost:${process.env.PORT}`)
+    console.log(`Servidor rodando em http://localhost:${process.env.PORT}`)
 });
